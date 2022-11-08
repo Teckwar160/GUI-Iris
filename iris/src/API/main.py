6b0cb9df6511bc33c756e09a760cb712654374dc
Teckwar160 <teckwar160@gmail.com>
2022-11-08 15:55:27 -0600

    pruebas de api

--- iris/src/API/main.py
+++ iris/src/API/main.py
@@ -6,9 +6,30 @@ data = pd.read_csv("pruebaAPI.csv")
 
 d = pd.DataFrame(data.dtypes)
 
-app = FastAPI()
+#app = FastAPI()
 
 
-@app.get("/Data")
+# @app.get("/Data")
 def download():
-    print(data.columns)
+
+    # with open("datos.js","w") as file:
+    #   file.write("export const data=")
+
+    # Arreglo con los nombres de las columnas
+    columnas = data.columns.values
+    print(columnas)
+
+    print("\n")
+
+    filas = data.index.values
+    print(filas)
+
+    print("\n")
+
+    #Obtener por fila los datos
+    for i in data.index.values:
+        for c in columnas:
+            print(data[c][i])
+
+
+download()
