81bf5f43ba1c3681246a7de7a0a75a4cb14008ed
Teckwar160 <teckwar160@gmail.com>
2022-11-08 19:01:51 -0600

    Componente tabla al 30%

--- iris/src/Componentes/Tabla.jsx
+++ iris/src/Componentes/Tabla.jsx
@@ -1,54 +1,58 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+import { dataColumnas, dataFilas } from "../API/datosEDA";
+function creaFilas(columnas, filas) {
+  // console.log(dataColumnas.length) //21
+  var filasRetorno = [];
+  var iterador = dataFilas.length;
+  var j = 0;
+  var tmp = {};
+  while (j < iterador) {
+    for (var i = 0; i < dataColumnas.length; i++) {
+      tmp[dataColumnas[i]] = dataFilas[j];
+      j++;
+    }
+    j++;
+    filasRetorno.push(tmp);
+    tmp = {};
   }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
+  return filasRetorno;
+}
 
-export default function Tabla(){
-    return (
-        <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
+const filas2 = creaFilas(dataColumnas, dataFilas);
+
+console.log(filas2);
+
+export default function Tabla() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {dataColumnas.map((columna, index) => (
+              <TableCell align="center">{columna}</TableCell>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filas2.map((fila, index) => (
+            <TableRow
+              key={fila.Suburb}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell>{fila.Suburb}</TableCell>
+              <TableCell>{fila.Address}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
